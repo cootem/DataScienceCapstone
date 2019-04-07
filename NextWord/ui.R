5c2c594e472f2a39c8147a926da6d235a35aace8
Michael Coote <cootemj@gmail.com>
2019-04-06 20:54:41 -0400

    first published ui

--- NextWord/ui.R
+++ NextWord/ui.R
@@ -12,16 +12,30 @@ shinyUI(
     fluidPage(
         headerPanel("Data Science Capstone"),
         titlePanel("Next Word Predictor"),
-        textInput("stem", "Enter a phrase", "type here"),
-        div(style="width:500px; margin-left:20px",
-            fluidRow( h3("My recomendations for the next word:") ),
-            fluidRow(
-                column(4, verbatimTextOutput("nextWord1")),
-                column(4, verbatimTextOutput("nextWord2")),
-                column(4, verbatimTextOutput("nextWord3")) 
+        div(style="
+            margin: 50px;
+            border-radius: 25px;
+            background-color: lightblue;
+            box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.75);",
+            br(),
+            div(style="
+                width: 800px; 
+                margin: 0 auto; 
+                color: red",
+            textInput("stem", "Enter a Phrase", "type here", width = "80%")
             ),
-            fluidRow( h3("Table of Probabilities"))
+            div(style="width:800px; 
+                margin:0 auto",
+                fluidRow( h3("My recomendations for the next word:") ),
+                fluidRow(
+                    column(4, verbatimTextOutput("nextWord1")),
+                    column(4, verbatimTextOutput("nextWord2")),
+                    column(4, verbatimTextOutput("nextWord3")) 
+                ),
+                fluidRow( h3("Known Stem, Next Word and Probabilities")),
+                fluidRow(dataTableOutput("yourNextWord"))
+            )
         ),
-        fluidRow(dataTableOutput("yourNextWord"))
+        br()
     )
 )
